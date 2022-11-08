4cb6274d4d637e483ab157df128ebe4bc40578fa
rourabpaul1986 <62745577+rourabpaul1986@users.noreply.github.com>
2022-11-08 15:53:23 +0530

    Update poly_add.vhd

--- PolyAdd/poly_add.vhd
+++ PolyAdd/poly_add.vhd
@@ -35,16 +35,20 @@ use work.rlwe_pkg.all;
 --use UNISIM.VComponents.all;
 
 entity poly_add is
-   generic(
-  rd_base_addr:     natural;
-  wr_base_addr:     natural
-   );
-    Port ( clk : in STD_LOGIC;
+--   generic(
+--  rd_base_addr:     natural range 0 to 255;
+--  wr_base_addr:     natural range 0 to 255
+--   );
+    Port ( 
+       rd_base_addr:  in   natural range 0 to 255;
+       wr_base_addr:  in    natural range 0 to 255;
+       clk : in STD_LOGIC;
        rst : in STD_LOGIC;
-       adder_op1: in  mult_type;
-       adder_op2: in  std_logic_vector(logq-1 downto 0);
+       adder_op1: in  mult_type;--from poly mult
+       adder_op2: in  std_logic_vector(logq-1 downto 0);--from memory
        sdmux :  out std_logic_vector(2 downto 0);
        mem_ctrl:  out  std_logic_vector(1 downto 0);
+       --offset : out STD_LOGIC_vector(logn-1 downto 0);
        addr :  out std_logic_vector(7 downto 0);
        adder_out: out  std_logic_vector(logq-1 downto 0);
        done : out STD_LOGIC);
@@ -55,12 +59,13 @@ signal addr_reg :  std_logic_vector(7 downto 0);
 signal add_done :  std_logic;
 signal op1, op2:   std_logic_vector(logq-1 downto 0);
 signal adder_out_reg:  mult_type;
-type add_state_type is (init, read, add, wrmem, addfin);
+type add_state_type is (init, read, add, wrmem, addfin,last);
 signal add_state       : add_state_type;
 begin
 addr<=addr_reg;
 addition_gen : process(clk, rst)--generate address
 begin
+--offset<=(others=>'0');
 if rising_edge(clk) then
     if(rst='1') then
      addr_reg<=std_logic_vector(to_unsigned(rd_base_addr, 8));
@@ -69,19 +74,18 @@ if rising_edge(clk) then
     else
      case add_state is
             when init =>  
-              mem_ctrl<="10";
-              sdmux<="011";
+              --mem_ctrl<="10";
+              --sdmux<="011";
               adder_out_reg<=(others=>(others=>'0'));
+              addr_reg<=std_logic_vector(to_unsigned(rd_base_addr, 8));
               add_done<='0';
               add_state<=read;
-            when read =>  
-              
+            when read =>               
               add_state<=add;
             when add =>    
-              --op1<=adder_op1(to_integer(unsigned(not addr_reg(logn-1 downto 0) ))mod n); 
-              --adder_out<=adder_op1(to_integer(unsigned(not addr_reg(3 downto 0)))mod n)+adder_op2;
-              adder_out_reg(to_integer(unsigned(addr_reg(logn-1 downto 0) )))<=adder_op1(to_integer(unsigned(not addr_reg(logn-1 downto 0))))+adder_op2;
-              if(addr_reg=base_addr_e+n-1) then
+              adder_out_reg(to_integer(unsigned(addr_reg(logn-1 downto 0) )))<=adder_op1(to_integer(unsigned( addr_reg(logn-1 downto 0))))+adder_op2;
+              --adder_out_reg(to_integer(unsigned(addr_reg(logn-1 downto 0) )))<=adder_op1(to_integer(unsigned(addr_reg(logn-1 downto 0))))+adder_op2;
+              if(addr_reg=rd_base_addr+n-1) then
                 add_state<=wrmem;
                 addr_reg<=std_logic_vector(to_unsigned(wr_base_addr, 8));
               else
@@ -92,15 +96,22 @@ if rising_edge(clk) then
                 adder_out<=adder_out_reg(to_integer(unsigned(addr_reg(logn-1 downto 0) )));
                 mem_ctrl<="11";
                 sdmux<="100";
-               if(addr_reg=base_addr_b+n-1) then
+               if(addr_reg=wr_base_addr+n) then
+                     mem_ctrl<="00";
                     add_state<=addfin;
                else
                     addr_reg<=addr_reg+1;
                     add_state<=wrmem;
                 end if;
                when addfin =>  
+                  mem_ctrl<="00";
                   add_done<='1';
-                  add_state<=addfin;
+                  add_state<=last;
+               when last =>  
+                   add_state<=last;
+                   add_done<='0';
+                   addr_reg<=(others=>'0');
+                     
            end case;
     end if;
 end if;
